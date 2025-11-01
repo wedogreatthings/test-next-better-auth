acb416a9f421563eca93e523ba55b0415b31f1f0
Fathismael93 <fathismael@gmail.com>
2025-11-01 15:39:57 +0300

    Checking if betterAuth is working and is creating the user in mongodb database

--- app/api/auth/[...all]/route.js
+++ app/api/auth/[...all]/route.js
@@ -1,3 +1,5 @@
 import { auth } from "@/lib/auth";
+import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = auth.handler;
+// 🔥 IMPORTANT : Utiliser toNextJsHandler et passer auth.handler
+export const { GET, POST } = toNextJsHandler(auth.handler);
